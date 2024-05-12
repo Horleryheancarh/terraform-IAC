d1599e2431ec47daa87107f6c8cb1c507d4c5b2f
Horleryheancarh <oladipoolayinka7@gmail.com>
2024-05-12 11:21:15 +0100

    add network acl

--- main.tf
+++ main.tf
@@ -66,3 +66,24 @@ resource "aws_vpc_security_group_ingress_rule" "allow_http_ipv6" {
 }
 
 # Create Network ACL
+resource "aws_network_acl" "main" {
+  vpc_id = aws_vpc.main.id
+
+  egress {
+    protocol   = "tcp"
+    rule_no    = 200
+    action     = "allow"
+    cidr_block = "10.0.1.0/24"
+    from_port  = 443
+    to_port    = 443
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "10.0.1.0/24"
+    from_port  = 80
+    to_port    = 80
+  }
+}
